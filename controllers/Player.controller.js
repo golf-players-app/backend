a4756b6c51a7effb5ccbd51dac430f3187950843
José Antonio Escudero García <jaeg90@gmail.com>
2023-06-25 16:47:25 +0200

    Player controller done

--- controllers/Player.controller.js
+++ controllers/Player.controller.js
@@ -61,23 +61,58 @@ module.exports.create = async (req, res, next) => {
   }
 };
 
+//module.exports.listByHandicap = async (req, res, next) => {};
+
 module.exports.detail = async (req, res, next) => {
   try {
-    const {id}
+    const { id } = req.params;
+    const player = await Player.findById(id);
+    return res.status(200).json(player);
   } catch (error) {
     next(error);
   }
 };
 
-module.exports.create = async (req, res, next) => {
+module.exports.update = async (req, res, next) => {
   try {
+    const { id } = req.params;
+    const player = await Player.findByIdAndUpdate(id, req.body, { new: true });
   } catch (error) {
     next(error);
   }
 };
 
-module.exports.create = async (req, res, next) => {
+module.exports.updatePassword = async (req, res, next) => {
   try {
+    const { id } = req.params;
+    const { password } = req.body;
+    const player = await Player.findById(id);
+    if (!bcrypt.compareSync(password, player.password)) {
+      res.status(400).json({
+        message: "Contraseña en uso, inserte una nueva contraseña",
+      });
+      return;
+    }
+    if (!password.test(regEx)) {
+      res.status(400).json({
+        message:
+          "La contraseña tiene que tener una longitud mínima de 6 caractéres con al menos una mayúscula, una minúsucla y un número",
+      });
+      return;
+    }
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashedSync(password, salt);
+    const player = await Player.findByIdAndUpdate(id, { password: hashedPassword }, { new: true });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.delete = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await Player.findByIdAndDelete(id);
+    return res.status(200).json({ message: "Cuenta eliminada correctamente" });
   } catch (error) {
     next(error);
   }
