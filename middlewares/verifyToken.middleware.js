46a451d541343c477071b01837d43694cc504e57
José Antonio Escudero García <jaeg90@gmail.com>
2023-07-01 10:12:23 +0200

    Backend 0.5

--- middlewares/verifyToken.middleware.js
+++ middlewares/verifyToken.middleware.js
@@ -3,21 +3,21 @@ const { expressjwt } = require("express-jwt");
 const isAuthenticated = expressjwt({
   secret: process.env.TOKEN_SECRET,
   algorithms: ["HS256"],
-  requestProperty: "payload"
-  // getToken: getTokenFromHeaders
+  requestProperty: "payload",
+  getToken: getTokenFromHeaders,
 });
 
-// function getTokenFromHeaders(req) {
-//   const authorizationHeader = req.get("Authorization");
-//   if (authorizationHeader) {
-//     const [bearer, token] = authorizationHeader.split(" ");
-//     if (bearer === "Bearer") {
-//       console.log("Token: ", token);
-//       return token;
-//     }
-//   }
+function getTokenFromHeaders(req) {
+  const authorizationHeader = req.get("Authorization");
+  if (authorizationHeader) {
+    const [bearer, token] = authorizationHeader.split(" ");
+    if (bearer === "Bearer") {
+      console.log("Token: ", token);
+      return token;
+    }
+  }
 
-//   return null;
-// }
+  return null;
+}
 
 module.exports = { isAuthenticated };
